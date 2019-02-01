56b8dc108a6d684bff6c1e44cc30a942096f0d66
Marko Ivancic <cicnavi@gmail.com>
2019-02-01 14:11:49 +0100

    Implement fixed array

--- src/Ds/Arrays/FixedArray.php
+++ src/Ds/Arrays/FixedArray.php
@@ -5,19 +5,144 @@ namespace Cicnavi\Ds\Arrays;
 
 use SplFixedArray;
 
+use InvalidArgumentException;
+use OutOfRangeException;
+
 class FixedArray 
 {
     protected $fixedArray;
     protected $capacity;
-    protected $size;
+    protected $size = 0;
 
 	/**
 	 * FixedArray constructor.
 	 *
 	 * @param int $capacity
 	 */
-	public function __construct(int $capacity = 0)
+	public function __construct(int $capacity = 1)
     {
+    	if ($capacity < 0) {
+    		throw new InvalidArgumentException("Invalid capacity.");
+	    }
+
+    	$this->capacity = $capacity;
+
         $this->fixedArray = new SplFixedArray($capacity);    
+    }
+
+	/**
+	 * Set value at specific index.
+	 *
+	 * @param int $index
+	 * @param mixed $value
+	 */
+    public function set(int $index, $value): void
+    {
+    	$this->validateIndex($index);
+
+	    $this->resolveSize($index);
+
+	    $this->fixedArray->offsetSet($index, $value);
+
+    }
+
+	/**
+	 * Add value to the end of the array.
+	 *
+	 * @param $value
+	 */
+    public function add($value): void
+    {
+    	$this->validateSize();
+
+		$this->fixedArray->offsetSet($this->size, $value);
+
+		$this->size++;
+    }
+
+	/**
+	 * Insert value to the specified index.
+	 *
+	 * @param int $index
+	 * @param $value
+	 */
+	public function insert(int $index, $value): void
+	{
+		$this->validateIndex($index);
+		$this->validateSize();
+
+		if ($index >= $this->size) {
+			$this->fixedArray->offsetSet($index, $value);
+			$this->resolveSize($index);
+		} else {
+			for($i = $this->size; $i > $index; $i--) {
+				$this->fixedArray->offsetSet($i, $this->fixedArray->offsetGet($i - 1));
+			}
+
+			$this->fixedArray->offsetSet($index, $value);
+
+			$this->size++;
+		}
+
+	}
+
+	/**
+	 * @param int $index
+	 *
+	 * @return mixed $value
+	 */
+    public function get(int $index)
+    {
+		$this->validateIndex($index);
+
+		return $this->fixedArray->offsetGet($index);
+    }
+
+	/**
+	 * Get current array size.
+	 *
+	 * @return int
+	 */
+    public function getSize(): int
+    {
+    	return $this->size;
+    }
+
+	/**
+	 * Get current array capacity.
+	 *
+	 * @return int
+	 */
+	public function getCapacity(): int
+	{
+		return $this->capacity;
+	}
+
+	/**
+	 * @param int $index
+	 *
+	 * @throws InvalidArgumentException
+	 */
+    protected function validateIndex(int $index)
+    {
+	    // Index must be smaller than capacity and at least 0.
+	    if ( $index >= $this->capacity || $index < 0 ) {
+		    throw new InvalidArgumentException("Invalid index.");
+	    }
+    }
+
+    protected function validateSize()
+    {
+	    if ($this->size >= $this->capacity) {
+		    throw new OutOfRangeException("Array is already full.");
+	    }
+    }
+
+    protected function resolveSize(int $index)
+    {
+	    // If current size is smaller than this index, indicate the new size.
+	    if ( $this->size <= $index ) {
+		    $this->size = $index + 1;
+	    }
     }
 }
--- tests/Ds/Arrays/FixedArrayTest.php
+++ tests/Ds/Arrays/FixedArrayTest.php
@@ -8,11 +8,98 @@ final class FixedArrayTest extends TestCase
 {
 	public function testCanCreateInstanceWithoutArguments()
 	{
-		$this->assertInstanceOf(FixedArray::class, new FixedArray());
+		$fixedArray = new FixedArray();
+		$this->assertInstanceOf(FixedArray::class, $fixedArray);
+		return $fixedArray;
+	}
+
+	public function testCanCreateInstanceWithZeroCapacity()
+	{
+		$fixedArray = new FixedArray(0);
+		$this->assertInstanceOf(FixedArray::class, $fixedArray);
+		return $fixedArray;
 	}
 
 	public function testCanCreateInstanceOfSpecificCapacity()
 	{
-		$this->assertInstanceOf(FixedArray::class, new FixedArray(10));
+		$fixedArray = new FixedArray(10);
+		$this->assertInstanceOf(FixedArray::class, $fixedArray);
+		return $fixedArray;
 	}
+
+	public function testCanNotCreateInstanceOfNegativeCapacity()
+	{
+		$this->expectException(InvalidArgumentException::class);
+
+		(new FixedArray(-1));
+	}
+
+	/**
+	 * @param FixedArray $fixedArray Array to check the size of.
+	 *
+	 * @depends testCanCreateInstanceOfSpecificCapacity
+	 */
+	public function testInitialSizeIsZero(FixedArray $fixedArray)
+	{
+		$this->assertEquals(0, $fixedArray->getSize());
+	}
+
+	/**
+	 * @param FixedArray $fixedArray Array to check the size of.
+	 *
+	 * @depends clone testCanCreateInstanceOfSpecificCapacity
+	 */
+	public function testSizeIsOneAfterFirstSet(FixedArray $fixedArray)
+	{
+		$fixedArray->set(0, 'First');
+
+		$this->assertEquals(1, $fixedArray->getSize());
+	}
+
+	/**
+	 * @param FixedArray $fixedArray Array to check the size of.
+	 *
+	 * @depends clone testCanCreateInstanceOfSpecificCapacity
+	 */
+	public function testCantAddIfArrayIsFull(FixedArray $fixedArray)
+	{
+		$this->expectException(OutOfRangeException::class);
+
+		$fixedArray->set(9, 'Last');
+
+		$fixedArray->add('One more');
+	}
+
+	/**
+	 * @param FixedArray $fixedArray Array to check the size of.
+	 *
+	 * @depends clone testCanCreateInstanceOfSpecificCapacity
+	 */
+	public function testCanInsertNewValue(FixedArray $fixedArray)
+	{
+		$value = 'Third';
+		$index = 2;
+
+		$fixedArray->insert($index, $value);
+
+		$this->assertEquals(3, $fixedArray->getSize());
+		$this->assertEquals($value, $fixedArray->get($index));
+	}
+
+	/**
+	 * @param FixedArray $fixedArray Array to check the size of.
+	 *
+	 * @depends clone testCanCreateInstanceWithZeroCapacity
+	 */
+	public function testCanNotInsertNewValueOnArrayWithZeroCapacity(FixedArray $fixedArray)
+	{
+		$this->expectException(InvalidArgumentException::class);
+
+		$value = 'First';
+		$index = 0;
+
+		$fixedArray->insert($index, $value);
+
+	}
+
 }
